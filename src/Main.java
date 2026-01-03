ccea1eca8b1b3af05df386189b517d6a16533a42
reemsuliman <reemsuliman2004@gmail.com>
2026-01-03 11:33:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,12 @@ import org.antlr.v4.runtime.tree.*;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        // الملفات التي نريد تحليلها
         String[] files = {
                 "templates/list.html",
                 "templates/add.html",
                 "templates/details.html"
         };
 
-        // نفس الـ SemanticVisitor لكل الملفات
         SemanticVisitor semanticVisitor = new SemanticVisitor();
 
         for (String file : files) {
@@ -24,30 +22,22 @@ public class Main {
             System.out.println("Parsing file: " + file);
             System.out.println("==============================");
 
-            // 1️⃣ اقرأ الملف
             CharStream input = CharStreams.fromFileName(file);
 
-            // 2️⃣ Lexer & Parser
             HtmlCssLexer lexer = new HtmlCssLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             HtmlCssParser parser = new HtmlCssParser(tokens);
 
-            // 3️⃣ Parse Tree
             ParseTree tree = parser.document();
 
-            // 4️⃣ AST Visitor
             JinjaVisitor astVisitor = new JinjaVisitor();
             Node ast = astVisitor.visit(tree);
 
-            // 5️⃣ طباعة AST
             System.out.println("===== AST =====");
             System.out.println(ast);
-
-            // 6️⃣ Semantic Visitor (يجمع الرموز)
             semanticVisitor.visit(tree);
         }
 
-        // 7️⃣ بعد الانتهاء من كل الملفات → اطبع جدول الرموز النهائي
         System.out.println("\n\n===== FINAL SYMBOL TABLE =====");
         semanticVisitor.getSymbolTable().print();
     }
